4b855ad90c1564b00240bc0601da3445e7ac2c98
siddhantgupta547 <sidgpt84@gmail.com>
2021-07-21 19:32:50 +0530

    Add path to routes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express= require('express');
 const app= express();
 const port= 8000;
+// const router= require('./routes');
+
+// to route every request to router
+app.use('/', require('./routes/index'));
+// app.use('/', 'router/index.js');
 
 app.listen(port, function(err){
     if(err){
